80482a21d5d10ee56712efa9b12315cd1551f743
andy3200 <149091611+andy3200@users.noreply.github.com>
2024-04-01 02:05:41 +0000

    fixing errors

--- src/hw3_main.c
+++ src/hw3_main.c
@@ -5,8 +5,9 @@
 
 int main(void) {
     static int num_tiles_placed;
-    GameState *game = initialize_game_state("./tests/boards/board02.txt");
-    game = place_tiles(game, 0, 0, 'H', "I", &num_tiles_placed);
+    GameState *game = initialize_game_state("./tests/boards/board05.txt"); 
+    game = place_tiles(game, 4, 4, 'H', "T G", &num_tiles_placed);
+    game = place_tiles(game, 5, 4, 'H', "H OK", &num_tiles_placed);
     save_game_state(game, "tests/actual_outputs/output");
     free_game_state(game);
     return 0;
