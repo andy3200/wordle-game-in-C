dcee75bc8cbc76b73c75389f6d2ef19a37d4d8ed
andy3200 <149091611+andy3200@users.noreply.github.com>
2024-04-01 00:31:48 +0000

    pushing it

--- src/hw3_main.c
+++ src/hw3_main.c
@@ -5,13 +5,11 @@
 
 int main(void) {
     static int num_tiles_placed;
-    GameState* game = initialize_game_state("tests/boards/board01.txt");
-    game = place_tiles(game, 2, 3, 'V', "T P", &num_tiles_placed);
-    game = place_tiles(game, 2, 3, 'V', "T P", &num_tiles_placed);
-    game = place_tiles(game, 2, 3, 'V', "T P", &num_tiles_placed);
-    game = place_tiles(game, 2, 3, 'V', "T P", &num_tiles_placed);
-    game = place_tiles(game, 2, 3, 'V', "T P", &num_tiles_placed);
-    save_game_state(game, "tests/actual_outputs/output1");
+    GameState *game = initialize_game_state("./tests/boards/board01.txt"); 
+    game = place_tiles(game, 2, 3, 'V', "T PMAN", &num_tiles_placed);
+    game = place_tiles(game, 2, 5, 'V', "P TAL", &num_tiles_placed);
+    game = place_tiles(game, 2, 5, 'V', "P TAL", &num_tiles_placed);
+    save_game_state(game, "tests/actual_outputs/output");
     free_game_state(game);
     return 0;
 }
